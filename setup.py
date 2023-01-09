4a6797f03fc670e6e558817491c01fccc734fea6
Syed Mahbub <xsmahmed776@gmail.com>
2023-01-10 03:32:53 +0600

    release version 0.3.8-dev3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='joyflo-sma',
     packages=['joyflo'],
-    version='0.3.8-dev2',
+    version='0.3.8-dev3',
     license='MIT',
     description='Some description',
     author='flojoy',
