6b164f7de30f0e36517925c84fb9e79244633b46
Syed Mahbub <xsmahmed776@gmail.com>
2023-04-08 19:31:42 +0100

    release dev version 0.2.2.dev3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='flojoy',
     packages=['flojoy'],
-    version='0.2.2.dev2',
+    version='0.2.2.dev3',
     license='MIT',
     description='Python client library for Flojoy.',
     author='flojoy',
