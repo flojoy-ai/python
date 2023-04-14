530bf89592a64a42023e2a45d1ea02879d15f16b
Syed Mahbub <xsmahmed776@gmail.com>
2023-04-14 17:11:27 +0100

    update dev version to 7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name='flojoy',
     packages=['flojoy'],
-    version='0.2.2.dev6',
+    version='0.2.2.dev7',
     license='MIT',
     description='Python client library for Flojoy.',
     author='flojoy',
