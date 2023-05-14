c0fc243f48600331fa9b48b917bed9d6a7f55161
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-05-14 14:11:30 -0400

    [WIP] added module scraper for scipy / numpy nodes

--- flojoy/__init__.py
+++ flojoy/__init__.py
@@ -3,3 +3,4 @@ from .job_result_builder import JobResultBuilder
 from .flojoy_instruction import FLOJOY_INSTRUCTION
 from .job_result_utils import *
 from .plotly_utils import *
+from .module_scraper import FlojoyWrapper
---
+++ flojoy/module_scraper.py
@@ -0,0 +1,189 @@
+import inspect
+
+class FlojoyWrapper:
+    CUSTOM_DOC_ADDITION = "-." * 36 + "\n\t"
+    CUSTOM_DOC_ADDITION += (
+        """The parameters of the function in this Flojoy wrapper are given below."""
+    )
+    CUSTOM_DOC_ADDITION += "\n\t" + "-." * 36 + "\n"
+    INPUT_DEFAULT_DTYPE = "np.ndarray"
+    FORBIDDEN_OPTIONAL_ARGS = ["x", "data", "kwargs", "comparator"]
+    FORBIDDEN_TYPES = ["tuple", "array-like", "array_like", "function", "callable", "sequence"]
+
+    def __init__(self, func, parameters, module, argument_names):
+        # we'll use parameters to get the actual default values of the arguments
+        # for generating the manifest
+        self.name = func.__name__
+        self.doc = func.__doc__
+        self.arguments = argument_names
+        self.optional_argument_dict = parameters
+        self.module = module
+        self.first_argument = argument_names[0]
+        self.parameters = {
+            self.first_argument: {"dtype": self.INPUT_DEFAULT_DTYPE, "optional": False}
+        }
+        self.data = f"from flojoy import DataContainer, flojoy\n"
+        self.data += f"import {module.__name__}\n\n"
+        self.data += f"@flojoy\ndef {self.name.upper()}(dc, params):\n\t"
+        
+        self.manifest = "COMMAND:\n\t- {\n"
+
+        self.process_docstring()
+
+    def process_docstring(self):
+        for idl, line in enumerate(self.doc.split("\n")):
+            if "Returns" in line:
+                break
+        self.doc = "\n" + (
+            "\n".join(
+                [
+                    ("\t" if (":" in line or "----------" in line) else "\t\t")
+                    + line.lstrip(" ")
+                    for line in self.doc.split("\n")[:idl]
+                ]
+            ).replace("\tParameters", self.CUSTOM_DOC_ADDITION + "\n" + "\tParameters")
+        )
+
+    def write_manifest(self):
+        self.manifest += "\t\t\tname: " + self.name + ",\n"
+        self.manifest += "\t\t\tkey: " + "TBD" + ",\n"
+        self.manifest += "\t\t\ttype: " + "TBD" + ",\n"
+        if self.parameters.keys() is not []:
+            self.manifest += "\t\t\tparameters: \n\t\t\t{\n"
+            for param in self.parameters.keys():
+                if param not in self.FORBIDDEN_OPTIONAL_ARGS:
+                    try:
+                        def_val = str(self.optional_argument_dict[param])
+                    except:
+                        def_val = "None"
+                    self.manifest += (
+                        f"\t\t\t\t{param}: "
+                        + "{"
+                        + "type: '"
+                        + str(self.parameters[param]["dtype"])
+                        + "', default: "
+                        + "'"
+                        + ("" if def_val == "None" else def_val)
+                        + "' },\n"
+                    )
+
+            self.manifest += "\t\t\t},\n\t}"
+
+    def write_wrapper(self):
+        if "callable" in self.doc:
+            # print(
+			# 	"#CANNOT PROCESS ",
+			# 	self.name,
+			# 	"since",
+			# 	"callable",
+			# 	"is not allowed ...",
+			# )
+            self.data = ""
+            self.manifest=""
+            return
+        self.data += """'''"""
+        self.data += self.doc
+        self.data += "\t" + """'''""" + "\n"
+        self.data += f"\treturn DataContainer(\n\t\tx=dc[0].y,\n\t\t"
+        self.data += f"y={self.module.__name__}.{self.name}(\n\t\t\t{self.first_argument}=dc[0].y,\n\t\t\t"
+        for idk, arg in enumerate(self.arguments):
+            if arg not in self.FORBIDDEN_OPTIONAL_ARGS:
+                dtype = ""
+                try:  # try to read it from the inspect dictionary
+                    dtype = type(self.optional_argument_dict[arg]).__name__
+                except:
+                    pass
+                if dtype == "" or dtype == "NoneType":
+                    for idl, line in enumerate(self.doc.split("\n")):
+                        if f"{arg} :" in line:
+                            dtype = (
+                                line.split(":")[1]
+                                .split(",")[0]
+                                .strip()
+                                .replace("}", "")
+                                .replace("{", "")
+                                .replace("(", "")
+                                .replace(")", "")
+                            )
+                            break
+                # now check for not allowed types after identifying them all correctly:
+                if dtype in self.FORBIDDEN_TYPES:
+                    # print(
+                    #     "#CANNOT PROCESS ",
+                    #     self.name,
+                    #     "since",
+                    #     dtype,
+                    #     "is not allowed ...",
+                    # )
+                    self.data = ""
+                    self.manifest=""
+                    return
+                if 'ndarray' in dtype:
+                    dtype = 'np.ndarray'
+                    self.data = "import numpy as np" + self.data
+                self.data += f"{arg}=({dtype}(params['{arg}']) if params['{arg}'] != '' else None),\n\t\t"
+
+                self.parameters[arg] = {"dtype": dtype, "optional": True}
+                self.data += "\t" if idk < len(self.arguments) - 1 else ""
+        self.data += ")\n\t)\n\n"
+        self.write_manifest()
+
+    def __repr__(self):
+        retval = f"#{self.name}, {self.parameters}\n"
+        retval += "#"+"-" * 72 + "\n#Wrapper\n" + "#"+"-" * 72 + "\n"
+        retval += self.data + "\n"
+        retval += "#"+"-" * 72 + "\n#Manifest\n" + "#"+"-" * 72 + "\n"
+        retval += "#"+self.manifest.replace("\n","\n#") + "\n"
+        return retval
+
+
+def scrape_function(func):
+    signature = inspect.signature(func)
+    param_names = [p.name for p in signature.parameters.values()]
+    default_optional_params = {
+        k: val.default
+        for k, val in signature.parameters.items()
+        if val.default != inspect.Parameter.empty
+    }
+    return func, param_names, default_optional_params
+
+if __name__ == "__main__":
+    import os 
+    from pathlib import Path
+    import scipy
+    import ast 
+
+    MODULES_TO_SCRAPE = [scipy.signal, scipy.stats]
+
+    CWD = os.getcwd()
+    MANIFEST_DIR = CWD / Path("manifest")
+    MANIFEST_DIR.mkdir(exist_ok=True)
+    invalids = []
+    valids = []
+    for module in MODULES_TO_SCRAPE:
+        NODE_DIR = Path(CWD) / f"{module.__name__}"
+        NODE_DIR.mkdir(exist_ok=True)
+
+        for name, func in inspect.getmembers(module, inspect.isfunction):
+            _, all_arg_names, default_optional_params = scrape_function(func)
+            if (all_arg_names[0] == "x" or all_arg_names[0] == "data") and (
+                "y" not in all_arg_names and "plot" not in all_arg_names
+            ):
+                fw = FlojoyWrapper(func, default_optional_params, module, all_arg_names)
+                fw.write_wrapper()
+                if fw.manifest != "" and fw.data != "" and "NoneType" not in fw.data:
+                    try:
+                        valid = ast.parse(fw.data)
+                        with open(NODE_DIR / f"{fw.name}.py", "w") as fh:
+                            fh.write(fw.data)
+                        with open(MANIFEST_DIR / f"{fw.name}.manifest.yaml", "w") as fh:
+                            fh.write(fw.manifest)
+                        valids.append(fw.name)
+                    except SyntaxError:
+                        invalids.append(fw.name)
+        with open(NODE_DIR/"__init__.py", "w") as fh:
+            functions = "__all__ = [" 
+            for name in valids:
+                functions += '"' + name.upper() + '", '
+            functions = functions[:-2] + "]\n"
+            fh.write(functions)
