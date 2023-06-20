9fda72f28ca16822c04e9270c8c5b2cab1d72388
Jinwon Lee <jinwonlee0916@gmail.com>
2023-06-20 18:37:30 -0400

    updated set_frontier_s3_key

--- flojoy/utils.py
+++ flojoy/utils.py
@@ -267,44 +267,27 @@ def set_frontier_api_key(api_key: str):
 
 
 def set_frontier_s3_key(s3Name: str, s3AccessKey: str, s3SecretKey: str):
-    try:
-        home = str(Path.home())
-        file_path = os.path.join(home, ".flojoy/credentials.yaml")
+    home = str(Path.home())
+    file_path = os.path.join(home, os.path.join(".flojoy", "credentials.yaml"))
 
-        data = {
-            f"{s3Name}": s3Name,
-            f"{s3Name}accessKey": s3AccessKey,
-            f"{s3Name}secretKey": s3SecretKey,
-        }
+    data = {
+        f"{s3Name}": s3Name,
+        f"{s3Name}accessKey": s3AccessKey,
+        f"{s3Name}secretKey": s3SecretKey,
+    }
+    if not os.path.exists(file_path):
+        # Create a new file and write the ACCSS_KEY to it
+        with open(file_path, "w") as file:
+            yaml.dump(data, file)
+        return
 
-        if not os.path.exists(file_path):
-            # Create a new file and write the ACCSS_KEY to it
-            with open(file_path, "w") as file:
-                yaml.dump(data, file)
-        else:
-            # Read the contents of the file
-            with open(file_path, "r") as file:
-                load = yaml.safe_load(file)
+    # Read the contents of the file
+    with open(file_path, "r") as file:
+        load = yaml.safe_load(file)
 
-            updated = False
-            for key, value in load.items():
-                if key == s3Name:
-                    load[key] = s3Name
-                    load[f"{s3Name}accessKey"] = s3AccessKey
-                    load[f"{s3Name}secretKey"] = s3SecretKey
-                    updated = True
-                    break
+    load[f"{s3Name}"] = s3Name
+    load[f"{s3Name}accessKey"] = s3AccessKey
+    load[f"{s3Name}secretKey"] = s3SecretKey
 
-            with open(file_path, "w") as file:
-                yaml.dump(load, file)
-
-            if not updated:
-                load[f"{s3Name}"] = s3Name
-                load[f"{s3Name}accessKey"] = s3AccessKey
-                load[f"{s3Name}secretKey"] = s3SecretKey
-
-                with open(file_path, "w") as file:
-                    yaml.dump(load, file)
-
-    except Exception as e:
-        raise e
+    with open(file_path, "w") as file:
+        yaml.dump(load, file)
