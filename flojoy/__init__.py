15a59320b1ede3f4a2982ea5f6a70da6525e7ab5
JeffDotPng <zhangjeff2014@gmail.com>
2023-06-26 09:58:06 -0400

    Export DataContainer types

--- flojoy/__init__.py
+++ flojoy/__init__.py
@@ -4,4 +4,22 @@ from .flojoy_instruction import FLOJOY_INSTRUCTION
 from .plotly_utils import *
 from .module_scraper import FlojoyWrapper
 from .job_result_utils import get_next_nodes, get_next_directions, get_job_result
-from .data_container import DataContainer
+from .data_container import (
+    DataContainer,
+    OrderedPair,
+    ParametricOrderedPair,
+    OrderedTriple,
+    ParametricOrderedTriple,
+    Scalar,
+    ParametricScalar,
+    Matrix,
+    ParametricMatrix,
+    DataFrame,
+    ParametricDataFrame,
+    Plotly,
+    ParametricPlotly,
+    Image,
+    ParametricImage,
+    Grayscale,
+    ParametricGrayscale,
+)
