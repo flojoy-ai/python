785dcd3f95d8f0e7014788fe9b5829c46b1c3e8c
Youngsun Yoo <youngsun123q@gmail.com>
2023-07-14 16:56:00 -0400

    added vector type support

--- flojoy/plotly_utils.py
+++ flojoy/plotly_utils.py
@@ -31,6 +31,8 @@ def data_container_to_plotly(data: DataContainer) -> dict[str, Any]:
             fig = px.scatter_3d(x=data_copy.x, y=data_copy.y, z=data_copy.z)
         case "scalar":
             fig = px.histogram(x=data_copy.c)
+        case "vector":
+            fig = px.histogram(x=data_copy.v)
         case "dataframe":
             df = cast(pd.DataFrame, data_copy.m)
             fig = go.Figure(
