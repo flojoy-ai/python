48b73a89197407087887f6dae6a180509862eb4b
dstrande <dstrande@live.ca>
2023-07-18 13:31:25 -0700

    dallas api autogen changes

--- flojoy/module_scraper.py
+++ flojoy/module_scraper.py
@@ -1,12 +1,8 @@
 import inspect
+import re
 
 
 class FlojoyWrapper:
-    CUSTOM_DOC_ADDITION = "-." * 36 + "\n\t"
-    CUSTOM_DOC_ADDITION += (
-        """The parameters of the function in this Flojoy wrapper are given below."""
-    )
-    CUSTOM_DOC_ADDITION += "\n\t" + "-." * 36 + "\n"
     INPUT_DEFAULT_DTYPE = "np.ndarray"
     FORBIDDEN_OPTIONAL_ARGS = ["x", "data", "kwargs", "comparator", "a", "A"]
     FORBIDDEN_TYPES = [
@@ -16,13 +12,22 @@ class FlojoyWrapper:
         "function",
         "callable",
         "sequence",
+        "... array_like",
+        "number or ndarray or sequence",
+        "complex"
     ]
 
     def __init__(self, func, parameters, module, argument_names):
-        # we'll use parameters to get the actual default values of the arguments
-        # for generating the manifest
+        # we'll use parameters to get the actual default values of the
+        # arguments for generating the manifest
         self.name = func.__name__
+        self.org_docs = func.__doc__
         self.doc = func.__doc__
+        self.count_returns()
+        # self.return_two = []
+        # if self.count_returns() != 1:
+        #     print(self.name, self.count_returns())
+        #     self.return_two.append(f'{self.name}: {self.count_returns()}')
         self.arguments = argument_names
         self.optional_argument_dict = parameters
         self.module = module
@@ -37,13 +42,75 @@ class FlojoyWrapper:
         except IndexError:  # this means argument_names is none
             self.first_argument = None
             self.parameters = {}
-        self.data = f"from flojoy import DataContainer, flojoy\n"
-        self.data += f"import {module.__name__}\n\n"
-        self.data += f"@flojoy\ndef {self.name.upper()}(dc, params):\n\t"
+        self.data = f"from flojoy import OrderedPair, flojoy, Matrix, Scalar\n"
+        self.data += f"import numpy as np\n"
+        self.data += f"from collections import namedtuple\n"
+        self.data += "from typing import Literal\n\n"
+        self.data += f"import {module.__name__}\n\n\n"
+        self.data += f"@flojoy(node_type='default')"
+        self.data += f"\ndef {self.name.upper()}(\n\t"
+        self.data += f"default: OrderedPair | Matrix,\n\t"
 
-        self.manifest = "COMMAND:\n\t- "
+        for idk, arg in enumerate(self.arguments):
+            if arg not in self.FORBIDDEN_OPTIONAL_ARGS:
+                dtype = ""
+                try:  # try to read it from the inspect dictionary
+                    dtype = type(self.optional_argument_dict[arg]).__name__
+                except:
+                    pass
 
+                try:
+                    def_val = str(self.optional_argument_dict[arg])
+                except:
+                    def_val = "None"
+                def_val = "" if def_val == "None" else def_val
+
+                if dtype == "" or dtype == "NoneType":
+                    for idl, line in enumerate(self.doc.split("\n")):
+                        if f"{arg} :" in line:
+                            dtype = (
+                                line.split(":")[1]
+                                .split(",")[0]
+                                .strip()
+                                .replace("}", "")
+                                .replace("{", "")
+                                .replace("(", "")
+                                .replace(")", "")
+                            )
+                            break
+                # now check for not allowed types after identifying them all:
+                if dtype in self.FORBIDDEN_TYPES:
+                    # print(
+                    #     "#CANNOT PROCESS ",
+                    #     self.name,
+                    #     "since",
+                    #     dtype,
+                    #     "is not allowed ...",
+                    # )
+                    self.data = ""
+                    return
+
+                if dtype == "str":
+                    def_val = f"'{def_val}'"
+
+                if dtype != "None" and def_val != "":
+                    self.data += f"{arg}: {dtype} = {def_val},\n\t"
+                elif dtype != "None" and def_val == "":
+                    self.data += f"{arg}: {dtype},\n\t"
+                else:
+                    self.data = ""
+
+                self.parameters[arg] = {"dtype": dtype, "optional": True}
+
+        if self.decomp_return:
+            self.gen_return_options()
+            self.data += f"select_return: Literal"
+            self.data += f"{[i for i in self.return_options]}"
+            self.data += f' = "{self.return_options[0]}",'
+
+        self.data += f") -> OrderedPair | Matrix | Scalar:\n\t"
         self.process_docstring()
+        self.write_test_script()
 
     def process_docstring(self):
         for idl, line in enumerate(self.doc.split("\n")):
@@ -56,73 +123,24 @@ class FlojoyWrapper:
                     + line.lstrip(" ")
                     for line in self.doc.split("\n")[:idl]
                 ]
-            ).replace("\tParameters", self.CUSTOM_DOC_ADDITION + "\n" + "\tParameters")
+            ).replace("\tParameters", "Parameters")
         )
 
-    def write_manifest(self, mtype):
-        self.manifest += "name: " + self.name.lower().capitalize() + "\n"
-        self.manifest += "\t\tkey: " + self.name.upper() + "\n"
-        self.manifest += "\t\ttype: " + mtype + "\n"
-        parameter_header = "\t\tparameters: \n\t\t"
-        parameter_string = ""
-        if self.parameters.keys() is not []:
-            for param in self.parameters.keys():
-                if param not in self.FORBIDDEN_OPTIONAL_ARGS:
-                    try:
-                        def_val = str(self.optional_argument_dict[param])
-                    except:
-                        def_val = "None"
-                    dtype = str(self.parameters[param]["dtype"])
-                    # sometimes we get 'None or float' ...
-                    if dtype == "str":
-                        dtype = "string"
-                    elif dtype == "None or float":
-                        dtype = "float"
-                    elif dtype == "bool":
-                        dtype = "boolean"
-                    parameter_string += (
-                        f"\t{param}: \n"
-                        + "\t\t\t\ttype: "
-                        + dtype
-                        + "\n\t\t\t\tdefault: "
-                        + ("" if def_val == "None" else def_val)
-                        + " \n\t\t"
-                    )
-            if parameter_string != "":
-                self.manifest += parameter_header + parameter_string
+        if self.decomp_return:
+            replace = "Parameters\n\t----------"
+            replace += "\n\tselect_return : This function has returns multiple"
+            replace += f" Objects:\n\t\t{self.return_options}. "
+            replace += "Select the desired one to return.\n\t\t"
+            replace += "See the respective function docs for descriptors."
+            self.doc = self.doc.replace("Parameters\n\t----------", replace)
 
-            self.manifest += "\t\t\n"
-        self.manifest = self.manifest.replace("\t", "  ")
+        self.doc = self.doc[:-2]
+        self.doc += "\n\tReturns\n\t----------\n\t"
+        self.doc += "DataContainer:\n\t\t"
+        self.doc += "type 'ordered pair', 'scalar', or 'matrix'"
+        self.doc = self.doc.replace("as follows:\n\n\t\t\n\t\t", "as follows:\n\n\t\t")
 
-    def write_wrapper(self, mtype):
-        if "callable" in self.doc:
-            # print(
-            # 	"#CANNOT PROCESS ",
-            # 	self.name,
-            # 	"since",
-            # 	"callable",
-            # 	"is not allowed ...",
-            # )
-            self.data = ""
-            self.manifest = ""
-            return
-        self.data += """'''"""
-        self.data += self.doc
-        self.data += "\t" + """'''""" + "\n"
-        self.data += f"\treturn DataContainer("
-        if self.module.__name__ != "numpy.matlib":
-            self.data += "\n\t\tx=dc[0].y,\n\t\t"
-            self.data += f"y={self.module.__name__}.{self.name}(\n\t\t\t" + (
-                f"{self.first_argument}=dc[0].y,\n\t\t\t"
-                if self.first_argument is not None
-                else ""
-            )
-        else:
-            self.data += f"\n\t\tm={self.module.__name__}.{self.name}(\n\t\t\t" + (
-                f"{self.first_argument}=dc[0].y,\n\t\t\t"
-                if self.first_argument is not None
-                else ""
-            )
+    def write_func_args(self):
         for idk, arg in enumerate(self.arguments):
             if arg not in self.FORBIDDEN_OPTIONAL_ARGS:
                 dtype = ""
@@ -153,25 +171,96 @@ class FlojoyWrapper:
                     #     "is not allowed ...",
                     # )
                     self.data = ""
-                    self.manifest = ""
-                    return
-                if "ndarray" in dtype:
-                    dtype = "np.ndarray"
-                    self.data = "import numpy as np" + self.data
-                self.data += f"{arg}=({dtype}(params['{arg}']) if params['{arg}'] != '' else None),\n\t\t"
 
-                self.parameters[arg] = {"dtype": dtype, "optional": True}
-                self.data += "\t" if idk < len(self.arguments) - 1 else ""
-        self.data += ")\n\t)\n\n"
-        self.write_manifest(mtype)
+                else:
+                    if "ndarray" in dtype:
+                        dtype = "np.ndarray"
+                        self.data = "import numpy as np" + self.data
+                    self.data += f"{arg}={arg},\n\t\t"
 
-    def __repr__(self):
-        retval = f"#{self.name}, {self.parameters}\n"
-        retval += "#" + "-" * 72 + "\n#Wrapper\n" + "#" + "-" * 72 + "\n"
-        retval += self.data + "\n"
-        retval += "#" + "-" * 72 + "\n#Manifest\n" + "#" + "-" * 72 + "\n"
-        retval += "#" + self.manifest.replace("\n", "\n#") + "\n"
-        return retval
+                    self.parameters[arg] = {"dtype": dtype, "optional": True}
+                    self.data += "\t" if idk < len(self.arguments) - 1 else ""
+
+    def gen_return_options(self):
+        self.return_options = re.findall(r'    (.*?) : ', self.return_doc)
+
+    def count_returns(self):
+        self.decomp_return = False
+        search = '    Returns\n    -------'
+        find1 = self.org_docs.find(search)
+        find2 = self.org_docs.find('-----', find1 + len(search))
+        self.return_doc = self.org_docs[find1:find2]
+        self.return_count = self.return_doc.count(' : ')
+        if self.return_count > 1:
+            self.decomp_return = True
+
+    def write_wrapper(self, mtype):
+        if "callable" in self.doc:
+            # print(
+            # 	"#CANNOT PROCESS ",
+            # 	self.name,
+            # 	"since",
+            # 	"callable",
+            # 	"is not allowed ...",
+            # )
+            self.data = ""
+            return
+        self.data += f"""'''The {self.name.upper()} node is based on a numpy or scipy function."""
+        self.data += """\n\tThe description of that function is as follows:\n"""
+        self.data += self.doc
+        self.data += "\n\t" + """'''""" + "\n\n"
+
+        if self.module.__name__ == "numpy.linalg":
+            self.data += f"\tresult = {self.module.__name__}.{self.name}(\n\t\t\t" + (
+                f"{self.first_argument}=default.m,\n\t\t\t"
+                if self.first_argument is not None
+                else ""
+            )
+
+            self.write_func_args()
+            self.data += ")\n"
+
+            if self.decomp_return:
+                self.data += "\n\tif isinstance(result, namedtuple):"
+                self.data += "\n\t\tresult = result._asdict()"
+                self.data += "\n\t\tresult = result[select_return]\n"
+
+            self.data += "\n\tif isinstance(result, np.ndarray):\n\t\t"
+            self.data += "result = Matrix(m=result)"
+            self.data += "\n\telif isinstance(result, np.float64):\n\t\t"
+            self.data += "result = Scalar(c=result)\n\t"
+
+        # elif self.module.__name__ == "numpy.matlib":  # TODO add matlib
+
+        else:
+            self.data += f"\tresult = OrderedPair(x=default.x,"
+            self.data += f"\n\t\ty={self.module.__name__}.{self.name}(\n\t\t\t" + (
+                f"{self.first_argument}=default.y,\n\t\t\t"
+                if self.first_argument is not None
+                else ""
+            )
+            self.write_func_args()
+            self.data += "))\n"
+
+        # self.data += ")\n\t)\n"
+        self.data += "\n\treturn result\n"
+
+    def write_test_script(self):
+        nodename = self.name.upper()
+
+        self.test_script = 'import numpy as np\n'
+        self.test_script += 'from flojoy import OrderedPair, Matrix, Scalar\n\n'
+        self.test_script += f'def test_{nodename}(mock_flojoy_decorator):'
+        self.test_script += f'\n\timport {nodename}\n\n\t'
+
+        if self.module.__name__ == "numpy.linalg":
+            self.test_script += 'element_a = Matrix(m=np.ones((5, 5)))'
+        else:
+            self.test_script += 'element_a = OrderedPair(x=np.ones(5), y=np.ones(5))'
+
+        self.test_script += f'\n\tres = {nodename}.{nodename}(default=element_a)'
+        self.test_script += '\n\n\t# check that the outputs are one of the correct types.'
+        self.test_script += 'assert isinstance(res, Scalar | OrderedPair | Matrix)\n'
 
 
 def scrape_function(func):
@@ -182,7 +271,6 @@ def scrape_function(func):
         for k, val in signature.parameters.items()
         if val.default != inspect.Parameter.empty
     }
-    print(func, param_names, default_optional_params)
     return func, param_names, default_optional_params
 
 
@@ -195,13 +283,11 @@ if __name__ == "__main__":
 
     MODULES_TO_SCRAPE = {
         "scipy": [scipy.signal, scipy.stats],
-        "numpy": [np.linalg, np.random.mtrand],
+        "numpy": [np.linalg],
     }
 
     CWD = os.getcwd()
-    MANIFEST_DIR = CWD / Path("MANIFEST")
-    MANIFEST_DIR.mkdir(exist_ok=True)
-
+    return_two = []
     for module in MODULES_TO_SCRAPE.keys():
         MODULE_DIR = Path(CWD) / Path(f"{module.upper()}")
         MODULE_DIR.mkdir(exist_ok=True)
@@ -212,11 +298,7 @@ if __name__ == "__main__":
             NODE_DIR = MODULE_DIR / Path(f"{submodule_name.upper()}")
             NODE_DIR.mkdir(exist_ok=True)
 
-            iterable = (
-                inspect.getmembers(submodule, inspect.isfunction)
-                if submodule_name != "random"
-                else inspect.getmembers(submodule)
-            )
+            # Use inspect to function params, arg names, etc.
             for name, func in inspect.getmembers(submodule, inspect.isfunction):
                 _, all_arg_names, default_optional_params = scrape_function(func)
                 # for the random library we need to check if there are any input arguments at all!
@@ -230,31 +312,50 @@ if __name__ == "__main__":
                         and "b" not in all_arg_names
                     ):
                         func_is_valid = True
+
+                # Create object (above) for each function
                 if func_is_valid:
                     fw = FlojoyWrapper(
                         func, default_optional_params, submodule, all_arg_names
                     )
                     fw.write_wrapper(f"{module.upper()}_{submodule_name.upper()}")
                     if (
-                        fw.manifest != ""
-                        and fw.data != ""
+                        fw.data != ""
                         and "NoneType" not in fw.data
                     ):
                         try:
                             valid = ast.parse(fw.data)
                             this_nodes_directory = Path(NODE_DIR / f"{fw.name.upper()}")
                             this_nodes_directory.mkdir(exist_ok=True)
+
+                            # Write node script.
                             with open(
                                 this_nodes_directory / f"{fw.name.upper()}.py", "w"
                             ) as fh:
                                 fh.write(fw.data)
-                            with open(
-                                MANIFEST_DIR / f"{fw.name}.manifest.yaml", "w"
-                            ) as fh:
-                                fh.write(fw.manifest)
+
+                            # Write testing script.
+                            # with open(
+                            #     this_nodes_directory / f"{fw.name.upper()}_test_.py", "w"
+                            # ) as fh:
+                            #     fh.write(fw.test_script)
+
                             valids.append(fw.name)
+
                         except SyntaxError:
                             invalids.append(fw.name)
+
+                        # Testing section: create nodes with errors.
+
+                        # this_nodes_directory = Path(NODE_DIR / f"{fw.name.upper()}")
+                        # this_nodes_directory.mkdir(exist_ok=True)
+                        # with open(
+                        #     this_nodes_directory / f"{fw.name.upper()}.py", "w"
+                        # ) as fh:
+                        #     fh.write(fw.data)
+
+            print('invalids: ', invalids)
+            print('valids: ', valids)
             with open(NODE_DIR / "__init__.py", "w") as fh:
                 functions = "__all__ = ["
                 for name in valids:
