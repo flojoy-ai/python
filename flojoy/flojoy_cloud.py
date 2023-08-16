3278e4b2b455680a5e8e297788dc3c8b210568ad
dstrande <dstrande@live.ca>
2023-08-16 13:22:20 -0700

    add pydantic for deserialize testing

--- flojoy/flojoy_cloud.py
+++ flojoy/flojoy_cloud.py
@@ -5,6 +5,9 @@ import os
 import requests
 import pandas as pd
 import numpy as np
+from pydantic import ValidationError, validator
+from typing import Optional, Generic, TypeVar
+from pydantic.generics import GenericModel
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -18,6 +21,126 @@ class NumpyEncoder(json.JSONEncoder):
         elif isinstance(obj, np.ndarray):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
+
+
+def check_deserialize(response):
+    DataC = TypeVar("DataC")
+
+    class DefaultModel(GenericModel, Generic[DataC]):
+        ref: str
+        dataContainer: dict
+        metadata: dict
+        workspaceId: str
+        privacy: str
+        location: str
+        note: str
+
+    class OrderedPairModel(DefaultModel[DataC], Generic[DataC]):
+        data: Optional[DataC]
+
+        @validator("dataContainer")
+        def type_must_match(cls, dc):
+            assert dc["type"] == "OrderedPair", "dataContainer type does not match."
+            return dc
+
+        @validator("dataContainer")
+        def keys_must_match(cls, dc):
+            assert "x" in dc, 'dataContainer does not contain "x" dataset.'
+            assert "y" in dc, 'dataContainer does not contain "y" dataset.'
+            assert isinstance(dc["x"], list), '"x" dataset is not a list'
+            assert isinstance(dc["y"], list), '"y" dataset is not a list'
+            return dc
+
+    class DataFrameModel(DefaultModel[DataC], Generic[DataC]):
+        data: Optional[DataC]
+
+        @validator("dataContainer")
+        def type_must_match(cls, dc):
+            assert dc["type"] == "DataFrame", "dataContainer type does not match."
+            return dc
+
+        @validator("dataContainer")
+        def keys_must_match(cls, dc):
+            assert "m" in dc, 'dataContainer does not contain "m" dataset.'
+            assert isinstance(
+                dc["m"], dict
+            ), f'"m" dataset is not a list, type: {type(dc["m"])}'
+            return dc
+
+    class MatrixModel(DefaultModel[DataC], Generic[DataC]):
+        data: Optional[DataC]
+
+        @validator("dataContainer")
+        def type_must_match(cls, dc):
+            assert dc["type"] == "Matrix", "dataContainer type does not match."
+            return dc
+
+        @validator("dataContainer")
+        def keys_must_match(cls, dc):
+            assert "m" in dc, 'dataContainer does not contain "m" dataset.'
+            assert isinstance(dc["m"], list), '"m" dataset is not a list'
+            return dc
+
+    class ScalarModel(DefaultModel[DataC], Generic[DataC]):
+        data: Optional[DataC]
+
+        @validator("dataContainer")
+        def type_must_match(cls, dc):
+            assert dc["type"] == "Scalar", "dataContainer type does not match."
+            return dc
+
+        @validator("dataContainer")
+        def keys_must_match(cls, dc):
+            assert "c" in dc, 'dataContainer does not contain "m" dataset.'
+            # int as well just in case?
+            assert isinstance(dc["c"], float), '"c" dataset is not a float'
+            return dc
+
+    class ImageModel(DefaultModel[DataC], Generic[DataC]):
+        data: Optional[DataC]
+
+        @validator("dataContainer")
+        def type_must_match(cls, dc):
+            assert dc["type"] == "Image", "dataContainer type does not match."
+            return dc
+
+        @validator("dataContainer")
+        def keys_must_match(cls, dc):
+            assert "r" in dc, 'dataContainer does not contain "r" dataset.'
+            assert "g" in dc, 'dataContainer does not contain "r" dataset.'
+            assert "b" in dc, 'dataContainer does not contain "r" dataset.'
+            assert isinstance(dc["r"], list), '"r" dataset is not a list'
+            assert isinstance(dc["g"], list), '"g" dataset is not a list'
+            assert isinstance(dc["b"], list), '"b" dataset is not a list'
+            return dc
+
+    dc_type = response["dataContainer"]["type"]
+    match dc_type:
+        case "OrderedPair":
+            try:
+                OrderedPairModel.parse_obj(response)
+            except ValidationError as e:
+                print(e)
+        case "DataFrame":
+            try:
+                DataFrameModel.parse_obj(response)
+            except ValidationError as e:
+                print(e)
+        case "Matrix":
+            try:
+                MatrixModel.parse_obj(response)
+            except ValidationError as e:
+                print(e)
+        case "Scalar":
+            try:
+                ScalarModel.parse_obj(response)
+            except ValidationError as e:
+                print(e)
+        case "Image":
+            try:
+                ImageModel.parse_obj(response)
+            except ValidationError as e:
+                print(e)
 
 
 class FlojoyCloud:
@@ -48,12 +171,12 @@ class FlojoyCloud:
         the Flojoy cloud client.
         """
         match dc_type:
-            case "ordered_pair":
+            case "OrderedPair":
                 if isinstance(data, dict) and "x" in data:
                     payload = json.dumps(
                         {
                             "data": {
-                                "type": "ordered_pair",
+                                "type": "OrderedPair",
                                 "x": data["x"],
                                 "y": data["y"],
                             }
@@ -67,21 +190,21 @@ class FlojoyCloud:
                     )
                     raise TypeError
 
-            case "dataframe":
+            case "DataFrame":
                 data = data.to_json()
-                payload = json.dumps({"data": {"type": "dataframe", "m": data}})
+                payload = json.dumps({"data": {"type": "DataFrame", "m": data}})
 
-            case "matrix":
+            case "Matrix":
                 payload = json.dumps(
-                    {"data": {"type": "matrix", "m": data}}, cls=NumpyEncoder
+                    {"data": {"type": "Matrix", "m": data}}, cls=NumpyEncoder
                 )
 
-            case "scalar":
+            case "Scalar":
                 payload = json.dumps(
-                    {"data": {"type": "scalar", "c": data}}, cls=NumpyEncoder
+                    {"data": {"type": "Scalar", "c": data}}, cls=NumpyEncoder
                 )
 
-            case "image":
+            case "Image":
                 RGB_img = np.asarray(data)
                 red_channel = RGB_img[:, :, 0]
                 green_channel = RGB_img[:, :, 1]
@@ -94,7 +217,7 @@ class FlojoyCloud:
                 payload = json.dumps(
                     {
                         "data": {
-                            "type": "image",
+                            "type": "Image",
                             "r": red_channel,
                             "g": green_channel,
                             "b": blue_channel,
@@ -121,7 +244,9 @@ class FlojoyCloud:
         """
         url = f"https://cloud.flojoy.ai/api/v1/dcs/{dc_id}"
         response = requests.request("GET", url, headers=self.headers)
-        return json.loads(response.text)
+        response = json.loads(response.text)
+        check_deserialize(response)
+        return response
 
     def to_python(self, dc):
         """
@@ -130,21 +255,21 @@ class FlojoyCloud:
         """
         dc_type = dc["dataContainer"]["type"]
         match dc_type:
-            case "ordered_pair":
+            case "OrderedPair":
                 df = pd.DataFrame(dc["dataContainer"])
                 return df.drop(columns=["type"])
 
-            case "dataframe":
+            case "DataFrame":
                 df = pd.DataFrame(dc["dataContainer"]["m"])
                 return df
 
-            case "matrix":
+            case "Matrix":
                 return pd.DataFrame(dc["dataContainer"]["m"])
 
-            case "scalar":
+            case "Scalar":
                 return float(dc["dataContainer"]["c"])
 
-            case "image":
+            case "Image":
                 image = dc["dataContainer"]
                 r = image["r"]
                 g = image["g"]
