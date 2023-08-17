4364f472765a5f3843cfaebbabc0a35f75c7396b
dstrande <dstrande@live.ca>
2023-08-17 13:57:52 -0700

    Update cloud for measurement

--- flojoy/flojoy_cloud.py
+++ flojoy/flojoy_cloud.py
@@ -51,6 +51,24 @@ def check_deserialize(response):
             assert isinstance(dc["y"], list), '"y" dataset is not a list'
             return dc
 
+    class OrderedTripleModel(DefaultModel[DataC], Generic[DataC]):
+        data: Optional[DataC]
+
+        @validator("dataContainer")
+        def type_must_match(cls, dc):
+            assert dc["type"] == "OrderedTriple", "dataContainer type does not match."
+            return dc
+
+        @validator("dataContainer")
+        def keys_must_match(cls, dc):
+            assert "x" in dc, 'dataContainer does not contain "x" dataset.'
+            assert "y" in dc, 'dataContainer does not contain "y" dataset.'
+            assert "x" in dc, 'dataContainer does not contain "z" dataset.'
+            assert isinstance(dc["x"], list), '"x" dataset is not a list'
+            assert isinstance(dc["y"], list), '"y" dataset is not a list'
+            assert isinstance(dc["z"], list), '"z" dataset is not a list'
+            return dc
+
     class DataFrameModel(DefaultModel[DataC], Generic[DataC]):
         data: Optional[DataC]
 
@@ -73,6 +91,20 @@ def check_deserialize(response):
         @validator("dataContainer")
         def type_must_match(cls, dc):
             assert dc["type"] == "Matrix", "dataContainer type does not match."
+            return dc
+
+        @validator("dataContainer")
+        def keys_must_match(cls, dc):
+            assert "m" in dc, 'dataContainer does not contain "m" dataset.'
+            assert isinstance(dc["m"], list), '"m" dataset is not a list'
+            return dc
+
+    class GrayscaleModel(DefaultModel[DataC], Generic[DataC]):
+        data: Optional[DataC]
+
+        @validator("dataContainer")
+        def type_must_match(cls, dc):
+            assert dc["type"] == "Grayscale", "dataContainer type does not match."
             return dc
 
         @validator("dataContainer")
@@ -121,6 +153,11 @@ def check_deserialize(response):
                 OrderedPairModel.parse_obj(response)
             except ValidationError as e:
                 print(e)
+        case "OrderedTriple":
+            try:
+                OrderedTripleModel.parse_obj(response)
+            except ValidationError as e:
+                print(e)
         case "DataFrame":
             try:
                 DataFrameModel.parse_obj(response)
@@ -129,6 +166,11 @@ def check_deserialize(response):
         case "Matrix":
             try:
                 MatrixModel.parse_obj(response)
+            except ValidationError as e:
+                print(e)
+        case "Grayscale":
+            try:
+                GrayscaleModel.parse_obj(response)
             except ValidationError as e:
                 print(e)
         case "Scalar":
@@ -164,12 +206,24 @@ class FlojoyCloud:
             pass
 
         self.headers = {"api_key": apikey, "Content-Type": content}
+        self.valid_types = [
+            "OrderedPair",
+            "OrderedTriple",
+            "DataFrame",
+            "Grayscale",
+            "Matrix",
+            "Scalar",
+            "Image",
+        ]
 
     def create_payload(self, data, dc_type):
         """
         A method that formats data into a payload that can be handled by
         the Flojoy cloud client.
         """
+        assert (
+            dc_type in self.valid_types
+        ), f"Type {dc_type} not supported. Check capitals (e.g. OrderedPair)."
         match dc_type:
             case "OrderedPair":
                 if isinstance(data, dict) and "x" in data:
@@ -189,21 +243,41 @@ class FlojoyCloud:
                         " dictionary form with keys 'x' and 'y'"
                     )
                     raise TypeError
-
+        match dc_type:
+            case "OrderedTriple":
+                if isinstance(data, dict) and "x" in data:
+                    payload = json.dumps(
+                        {
+                            "data": {
+                                "type": "OrderedTriple",
+                                "x": data["x"],
+                                "y": data["y"],
+                                "z": data["z"],
+                            }
+                        },
+                        cls=NumpyEncoder,
+                    )
+                else:
+                    print(
+                        "For ordered triple type, data must be in"
+                        " dictionary form with keys 'x', 'y', and 'z'"
+                    )
+                    raise TypeError
             case "DataFrame":
                 data = data.to_json()
                 payload = json.dumps({"data": {"type": "DataFrame", "m": data}})
-
             case "Matrix":
                 payload = json.dumps(
                     {"data": {"type": "Matrix", "m": data}}, cls=NumpyEncoder
                 )
-
+            case "Grayscale":
+                payload = json.dumps(
+                    {"data": {"type": "Grayscale", "m": data}}, cls=NumpyEncoder
+                )
             case "Scalar":
                 payload = json.dumps(
                     {"data": {"type": "Scalar", "c": data}}, cls=NumpyEncoder
                 )
-
             case "Image":
                 RGB_img = np.asarray(data)
                 red_channel = RGB_img[:, :, 0]
@@ -258,17 +332,14 @@ class FlojoyCloud:
             case "OrderedPair":
                 df = pd.DataFrame(dc["dataContainer"])
                 return df.drop(columns=["type"])
-
-            case "DataFrame":
+            case "OrderedTriple":
+                df = pd.DataFrame(dc["dataContainer"])
+                return df.drop(columns=["type"])
+            case "DataFrame" | "Matrix" | "Grayscale":
                 df = pd.DataFrame(dc["dataContainer"]["m"])
                 return df
-
-            case "Matrix":
-                return pd.DataFrame(dc["dataContainer"]["m"])
-
             case "Scalar":
                 return float(dc["dataContainer"]["c"])
-
             case "Image":
                 image = dc["dataContainer"]
                 r = image["r"]
@@ -281,3 +352,58 @@ class FlojoyCloud:
                 else:
                     img_combined = np.stack((r, g, b), axis=2)
                     return Image.fromarray(np.uint8(img_combined)).convert("RGB")
+
+    def list_dcs(self, size=10):
+        """
+        A method that lists the number of DataContainers specified.
+
+        If the number of DataContainers is less than the specified number,
+        an error will be thrown.
+        """
+        url = f"https://cloud.flojoy.ai/api/v1/dcs/?inbox=true&size={size}"
+        response = requests.request("GET", url, headers=self.headers, data="")
+        response = json.loads(response.text)
+        try:
+            response = response["data"]
+        except KeyError:
+            print("Size larger than the number of DataContainers.")
+
+        return response
+
+    def create_measurement(self, name, privacy="private"):
+        """
+        A method that creates a measurements with the name specified.
+        """
+        url = "https://cloud.flojoy.ai/api/v1/measurements"
+        payload = json.dumps({"name": name, "privacy": privacy})
+        response = requests.request("GET", url, headers=self.headers, data=payload)
+        response = json.loads(response.text)
+
+        return response
+
+    def list_measurements(self, size=10):
+        """
+        A method that lists the number of measurements specified.
+
+        If the number of measurements is less than the specified number,
+        an error will be thrown.
+        """
+        url = f"https://cloud.flojoy.ai/api/v1/measurements/?size={size}"
+        response = requests.request("GET", url, headers=self.headers, data="")
+        response = json.loads(response.text)
+        try:
+            response = response["data"]
+        except KeyError:
+            print("Size larger than the number of DataContainers.")
+
+        return response
+
+    def get_measurement(self, meas_id):
+        """
+        A method fetchs measurements from the client.
+        """
+        url = f"https://cloud.flojoy.ai/api/v1/measurements/{meas_id}"
+        response = requests.request("GET", url, headers=self.headers, data="")
+        response = json.loads(response.text)
+
+        return response
