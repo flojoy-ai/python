2200bfe07b594053a64a09a80bf3c28bbb620705
JeffDotPng <zhangjeff2014@gmail.com>
2023-08-28 22:34:40 -0400

    add comment

--- flojoy/plotly_utils.py
+++ flojoy/plotly_utils.py
@@ -32,6 +32,8 @@ def data_container_to_plotly(data: DataContainer) -> dict[str, Any] | None:
         case "Scalar":
             fig.add_trace(
                 go.Indicator(
+                    # FIXME: Remove this hack to fix the issue of
+                    # nodes not actually returning a float for scalar values
                     value=data_copy.c
                     if isinstance(data_copy.c, float)
                     else data_copy.c[0],
