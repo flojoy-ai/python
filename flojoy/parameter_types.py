d618316e0fb7410b247fdb9f190afae88345058d
JeffDotPng <zhangjeff2014@gmail.com>
2023-09-20 14:30:27 -0400

    just cast

--- flojoy/parameter_types.py
+++ flojoy/parameter_types.py
@@ -31,15 +31,13 @@ class CameraDevice(HardwareDevice):
 
 
 class SerialDevice(HardwareDevice):
-    # alias for clarity
     def get_port(self):
-        return self.get_id()
+        return str(self.get_id())
 
 
 class VisaDevice(HardwareDevice):
-    # alias for clarity
     def get_address(self):
-        return self.get_id()
+        return str(self.get_id())
 
 
 class CameraConnection(HardwareConnection):
